0850b876db6451fd6b93c4f947677efd16ac93af
nassar <assarn@objectcomputing.com>
2023-04-27 12:06:20 -0500

    first

---
+++ src/main/kotlin/com/virtualpairprogrammers/theater/ServletInitializer.kt
@@ -0,0 +1,12 @@
+package com.virtualpairprogrammers.theater
+
+import org.springframework.boot.builder.SpringApplicationBuilder
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer
+
+class ServletInitializer : SpringBootServletInitializer() {
+
+	override fun configure(application: SpringApplicationBuilder): SpringApplicationBuilder {
+		return application.sources(TheaterApplication::class.java)
+	}
+
+}
